b03d14a15a5e0785dc6f3ecc6fbbecc87a0e6826
Hung Tran Manh <tranmanhhung@tma.com.vn>
2020-10-01 11:19:27 +0700

    modify to run on Windows machine

--- src/test/java/nt/sample/base/BaseTest.java
+++ src/test/java/nt/sample/base/BaseTest.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+
+import lib.Util;
 import pageObjects.HomePage;
 
 public class BaseTest {
@@ -12,7 +14,16 @@ public class BaseTest {
 
     @BeforeClass
     public void setUp(){
-        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        switch(Util.getOS()){
+            case WINDOWS:
+                break;
+            case LINUX:
+                System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+                break;
+            default:
+                System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        }
+        
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
         homePage = new HomePage(driver);
