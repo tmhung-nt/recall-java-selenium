9798a018fa6bff1db79eee3aa9ce70c8b4d94385
Hung Tran Manh <tranmanhhung@tma.com.vn>
2020-10-01 17:03:45 +0700

    fix fail test due to wrong setUp

--- src/test/java/nt/sample/base/BaseTest.java
+++ src/test/java/nt/sample/base/BaseTest.java
@@ -3,16 +3,18 @@ package nt.sample.base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 
 import lib.Util;
+import org.testng.annotations.BeforeTest;
 import pageObjects.HomePage;
 
 public class BaseTest {
     private WebDriver driver;
     protected HomePage homePage;
 
-    @BeforeClass
+    @BeforeTest
     public void setUp(){
         switch(Util.getOS()){
             case WINDOWS:
@@ -23,13 +25,13 @@ public class BaseTest {
             default:
                 System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
         }
-        
+
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
         homePage = new HomePage(driver);
     }
 
-    @AfterClass
+    @AfterTest
     public void tearDown(){
         driver.close();
     }
