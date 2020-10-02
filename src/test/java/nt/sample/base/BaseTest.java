b67d7e5cd01bc079589306cc6931368e13c59be7
Hung Tran <tmhung.nt@gmail.com>
2020-10-03 06:32:19 +0700

    take screenshot on failure

--- src/test/java/nt/sample/base/BaseTest.java
+++ src/test/java/nt/sample/base/BaseTest.java
@@ -1,12 +1,18 @@
 package nt.sample.base;
 
+import com.google.common.io.Files;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.*;
 import utils.CommonUtils;
 import pageObjects.HomePage;
 import utils.WindowManager;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
@@ -38,6 +44,20 @@ public class BaseTest {
     @AfterClass
     public void tearDown(){
         driver.quit();
+    }
+
+    @AfterMethod
+    public void recordFailure(ITestResult testResult){
+        if (testResult.getStatus() == ITestResult.FAILURE){
+            var camera = (TakesScreenshot)driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            try {
+                Files.move(screenshot, new File("screenshots/" + testResult.getName() + ".png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Screenshot taken: " + screenshot.getAbsolutePath());
+        }
     }
 
     public WindowManager getWindowManager(){
